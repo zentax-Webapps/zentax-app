b91d7bdb3463f347b5f6ae358f722c3b951e27c4
zentax-Webapps <zentaxaimodels@gmail.com>
2026-06-15 16:44:33 +0530

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Service worker for PWA install + offline shell cache.
-const CACHE = 'zentax-v2';
+const CACHE = 'zentax-v3';
 const SHELL = [
   '/', '/index.html', '/manifest.json',
   '/css/styles.css',
